5bd6cf66b57149e7d81d4b73c863adfb9f4246c5
wallacewd <danwallaceasu@gmail.com>
2021-11-16 00:35:28 -0500

    dApp Update

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
 import contract from "../contracts/contract.json";
-import Hero from "../assets/hero.png";
+import Hero from "../assets/phunkhero.png";
 
 const initialInfoState = {
   connected: false,
